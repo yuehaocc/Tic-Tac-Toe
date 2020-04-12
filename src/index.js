283a24debcc13e00e617aa8a061c65a07a2a534c
yuehao <879389355@qq.com>
2020-04-12 21:20:07 +0800

    optimize hardcode in borad component

--- src/index.js
+++ src/index.js
@@ -2,46 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Square(props) {
-  return (
-    <button
-      className="square"
-      onClick={props.onClick}
-    >
-      {props.value}
-    </button>
-  );
-}
-
 class Board extends React.Component {
-
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.data[i]}
-        onClick={() => this.props.onClick(i)} />
-    );
-  }
-
   render() {
-
-
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+        <div className="board">
+          {
+            Array(9).fill(null).map((item, index) => (
+              <button
+                className="square"
+                onClick={() => this.props.onClick(index)}
+                key={index}
+              >
+                {this.props.data[index]}
+              </button>
+            ))
+          }
         </div>
       </div>
     );
